0d7aa53b2eff6ca25624edcc3cf778982cf81a4d
themurddock <playmurddock@gmail.com>
2016-02-24 11:26:13 -0500

    Revert "cambio 6..."
    This reverts commit ce92c2729b531c07fb2d31b45b044b71f3e14966.

--- build/mm.bundle.js
+++ build/mm.bundle.js
@@ -5612,9 +5612,6 @@ angular.module('mm.core.login', [])
         url: '/site',
         templateUrl: 'core/components/login/templates/site.html',
         controller: 'mmLoginSiteCtrl'
-        onEnter: function($state) {
-$state.go('mm_login.credentials', {siteurl: 'cordemaria.milaulas.com'});  // (here is my institution Moodle URL)
-} 
     })
     .state('mm_login.credentials', {
         url: '/cred',
